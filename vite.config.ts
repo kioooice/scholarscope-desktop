7cece9a84fd3afbdf7c52fca9c3f755eea82e703
Administrator <admin@example.com>
2026-08-24 08:55:28 +0800

    fix: move local web server to port 5180

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '127.0.0.1',
+    port: 5180,
+    strictPort: true,
     proxy: {
       '/api/openaire': {
         target: 'https://api.openaire.eu',
